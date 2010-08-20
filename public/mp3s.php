a3f0765bbce4d4e9333fdda9b64fa4ff665ae947
orta <orta@wgrids.com>
2010-08-20 18:55:47 +0100

    moved big files to dropbox!

--- public/mp3s.php
+++ public/mp3s.php
@@ -14,22 +14,22 @@
     <a href="index.php"><img src="home.gif" border=0></a><br><br>
     
     <div class="h_half">
-          <a href="audio/Dildano.Crocodileopolis.zip" class="audio">
+          <a href="http://dl.dropbox.com/u/251829/a%20disappointing%20evening%20with.zip" class="audio">
     	A Dissapointing Evening with...
           </a><br><br><br>
       
-          <a href="audio/Dildano.Crocodileopolis.zip" class="audio">
+          <a href="http://dl.dropbox.com/u/251829/Dildano.Crocodileopolis.zip" class="audio">
     	Crocodileopolis
           </a><br><br><br>
       
-              <a href="audio/SKT.mp3" class="audio">
+              <a href="http://dl.dropbox.com/u/251829/SKT.mp3" class="audio">
         	SuperKennyTape
               </a><br><br><br>
       
       <a href="audio/SKT.mp3" class="audio">
 	SuperKennyTape
       </a><br><br><br>
-      <a href="audio/Dildano%20-%20The%20Buzzard%20Maze.zip" class="audio">
+      <a href="http://dl.dropbox.com/u/251829/Dildano%20-%20The%20Buzzard%20Maze.zip" class="audio">
 	The Buzzard Maze
       </a><br><br><br>
     </div>
